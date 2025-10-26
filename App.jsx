eabf3654e3c855517dbde89e3c71b26cd2b1cede
dmonster5150-droid <dmonster5150@gmail.com>
2025-10-26 02:15:15 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,22 @@
 
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
+import Profile from "./pages/Profile";
+
+export default function App() {
+  return (
+    <div className="app">
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/jobs">Jobs</Link> |{" "}
+        <Link to="/profile">Profile</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
+  );
+}
